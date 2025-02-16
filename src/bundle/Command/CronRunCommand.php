cfd92d28ec6331418b4b440ee6ac9b79503080b8
Adam Wójs <adam@wojs.pl>
2025-02-16 17:17:35 +0100

    [CLI] Replaced deprecated Command::{$defaultName, $defaultDescription} with the AsCommand attribute

--- src/bundle/Command/CronRunCommand.php
+++ src/bundle/Command/CronRunCommand.php
@@ -14,11 +14,13 @@ use Cron\Report\CronReport;
 use Cron\Resolver\ArrayResolver;
 use Ibexa\Bundle\Cron\Registry\CronJobsRegistry;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'ibexa:cron:run', description: 'Perform one-time cron tasks run.')]
 final class CronRunCommand extends Command
 {
     private CronJobsRegistry $cronJobsRegistry;
@@ -39,8 +41,6 @@ final class CronRunCommand extends Command
             ->setDefinition([
                 new InputOption('category', null, InputOption::VALUE_REQUIRED, 'Job category to run', 'default'),
             ])
-            ->setName('ibexa:cron:run')
-            ->setDescription('Perform one-time cron tasks run.')
             ->setHelp(
                 <<<EOT
 It's not meant to be run manually, yet it's OK to do so as it still might be useful for development purpose.
