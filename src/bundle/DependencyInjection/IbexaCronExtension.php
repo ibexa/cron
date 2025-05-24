67c2773296d3de5c48830c8afa7b275d2f604ae9
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-05-24 13:01:25 +0200

    Replaced internal HttpKernel Extension usage in IbexaCronExtension (#25)

--- src/bundle/DependencyInjection/IbexaCronExtension.php
+++ src/bundle/DependencyInjection/IbexaCronExtension.php
@@ -9,9 +9,9 @@ namespace Ibexa\Bundle\Cron\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
 /**
